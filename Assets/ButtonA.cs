242bca95773796812b72f26eb39f60a3f947d0cc
ttaj <catastronig@yahoo.com>
2019-04-23 20:44:52 -0500

    Initial upload

---
+++ Assets/ButtonA.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonA : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject button;
+	public GameObject page;
+	public MagazineHandler r;
+	public bool right = false;
+
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+		if (right == false)
+			r.TurnPageLeft();
+		else
+			r.TurnPageRight();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		button.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MagazineHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagazineHandler : MonoBehaviour {
+	public Texture[] textures;
+	private int currentPage1 = 0, currentPage2 = 1;
+	public GameObject page1, page2;
+
+
+	public void TurnPageLeft()
+	{
+		currentPage1--;
+		currentPage2--;
+
+		if(currentPage1 < 0)
+		{
+			currentPage1 = textures.Length - 1;
+		}
+
+		if(currentPage2 < 0)
+		{
+			currentPage2 = textures.Length - 1;
+		}
+
+		SetTextures();
+	}
+
+	public void TurnPageRight()
+	{
+		currentPage1++;
+		currentPage2++;
+
+		if (currentPage1 >= textures.Length)
+			currentPage1 = 0;
+		if (currentPage2 >= textures.Length)
+			currentPage2 = 0;
+
+		SetTextures();
+	}
+
+	public void SetTextures()
+	{
+		page1.GetComponent<MeshRenderer>().material.mainTexture = textures[currentPage1];
+		page2.GetComponent<MeshRenderer>().material.mainTexture = textures[currentPage2];
+		Debug.Log("HIT!!!!! " + currentPage1 + " " + currentPage2);
+	}
+
+	// Use this for initialization
+	void Start () {
+		SetTextures();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
